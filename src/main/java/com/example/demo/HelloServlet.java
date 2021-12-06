03e55bdaea1de925352101bedbffbfdfed101ce6
Sarwan Kr. Sharma <sarwan.sharma@hotmail.com>
2021-12-06 14:39:07 +0530

    Update HelloServlet.java

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello India!";
+        message = "Hello Srilanka!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
